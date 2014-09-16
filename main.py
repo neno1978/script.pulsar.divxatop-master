f0fdf4ecde107fdf21bf9ac678410a485d27cd43
neno1978 <titinoytitina.marcob@me.com>
2014-09-16 18:31:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import urllib2
 PAYLOAD = json.loads(base64.b64decode(sys.argv[1]))
 
 def search(query):
-    response = urllib2.urlopen("http://cuelgame.net/search.php?q=%s" % urllib.quote_plus(query))
+    response = urllib2.urlopen("http://cuelgame.net/search.php?q=malefica" % urllib.quote_plus(query))
     data = response.read()
     if response.headers.get("Content-Encoding", "") == "gzip":
         import zlib
